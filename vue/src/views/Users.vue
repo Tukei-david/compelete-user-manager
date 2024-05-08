3473f0570653517396f7c71654457888890bcbec
Tukei-david <dtukei831@gmail.com>
2024-05-08 11:41:59 +0300

    User table display

--- vue/src/views/Users.vue
+++ vue/src/views/Users.vue
@@ -1,11 +1,12 @@
 <template>
     <PageComponent title="Users">
-
+        <UsersTable/>
     </PageComponent>
 </template>
 
 <script setup>
 
 import PageComponent from '../components/layouts/PageComponent.vue';
+import UsersTable from '../components/UsersTable.vue';
 
 </script>

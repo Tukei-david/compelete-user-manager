90530147a95e43e069ab7109022e129d5d01ce90
Tukei-david <dtukei831@gmail.com>
2024-05-13 21:38:33 +0300

    Create and update user backend complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -27,9 +30,20 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateUserRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data['is)admin'] = true;
+        $data['email_verified_at'] = date('Y-m-d H:i:s');
+        $data['password'] = Hash::make($data['password']);
+
+
+        $data['created_by'] = $request->user()->id;
+        $data['updated_by'] = $request->user()->id;
+
+        $user = User::create($data);
+
+        return new UserResource($user);
     }
 
     /**
@@ -51,9 +65,19 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        $data = $request->validated();
+
+        if (!empty($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+
+        $data['updated_by'] = $request->user()->id;
+
+        $user->update($data);
+
+        return new UserResource($user);
     }
 
     /**
